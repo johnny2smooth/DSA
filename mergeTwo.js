799e596d6976e9c2262349c2ce2832b32511fe4e
johnny2smooth <johnbp13@gmail.com>
2022-04-18 16:39:16 -0700

    wrong way

--- mergeTwo.js
+++ mergeTwo.js
@@ -1,1 +1,18 @@
-// Yo!
+// My way. Probably not too *fast*
+// const mergeTwoLists = function(list1, list2) {
+//     return [...list1, ...list2].sort((a,b) => a-b)
+// };
+
+// 
+const mergeTwoLists = function(list1, list2) {
+  console.log(list1)
+  console.log(list2)
+  for(let i = 0; i < 1; i++){
+    if(list1[i] > list2[i]){
+
+    }
+  }
+  return true
+};
+
+console.log(mergeTwoLists([1,2,4], [1,3,5]))
