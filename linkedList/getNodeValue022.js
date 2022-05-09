1809e80b0f57bb8741e4f12928c04e04d15c1217
johnny2smooth <johnbp13@gmail.com>
2022-05-09 10:41:29 -0700

    add his solution

--- linkedList/getNodeValue022.js
+++ linkedList/getNodeValue022.js
@@ -21,8 +21,21 @@
 //   return null;
 // };
 
-const getNodeValue = (head, index) => {
-  if (head === null) return null;
-  if (index === 0) return head.val;
-  return getNodeValue(head.next, index - 1);
-};
+// recursive
+// const getNodeValue = (head, index) => {
+//   if (head === null) return null;
+//   if (index === 0) return head.val;
+//   return getNodeValue(head.next, index - 1);
+// };
+
+// his iterative solution
+// const getNodeValue = (head, index) => {
+//   let count = 0;
+//   let current = head;
+//   while (current !== null) {
+//     if (count === index) return current.val;
+//     current = current.next;
+//     count += 1;
+//   }
+//   return null;
+// };
