dab045d7b78d5ed933432acd65e567c31bb5fc22
johnny2smooth <johnbp13@gmail.com>
2022-05-19 12:00:53 -0700

    treeIncludes prep, overview

--- binaryTree/treeIncludes.js
+++ binaryTree/treeIncludes.js
@@ -9,6 +9,44 @@
 //   }
 // }
 
+// parameters
+// root node + target value
+// root node can be null?
+// target value will always be a number?
+// root.val will always be a number (unless null)
+// guarenteed a binary tree?
+
+// returns
+// boolean
+// either true or false
+
+// example
+// build a node...
+// add its path...
+// treeIncludes(a, 22)
+// true
+// treeIncludes(a, 412355161)
+// false
+// treeIncludes(null, 22)
+// false
+
+// psuedo code
+// set up function
+// edge case for root === null;
+// set up our stack array
+// loop while stack > 0
+// pop current node off stack
+// check currentnode.value
+// if that value is === target return true
+// push children to the stack if they're !== null
+// return false if all else fail
+
+// recursive psuedo
+// check for edge case and final leaves case
+// check to see if current node.val === target
+// if not return a recursive call to left or right side. if either side evals to true,
+// we will return true
+
 const treeIncludesQueue = (root, target) => {
   if (root === null) return false;
   const queue = [root];
