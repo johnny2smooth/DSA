1dd960e3a9cd412e5389a726d60e23da69552eb1
johnny2smooth <johnbp13@gmail.com>
2022-05-23 08:56:18 -0700

    solution added. prep still needed

---
+++ binaryTree/howHigh045.js
@@ -0,0 +1,23 @@
+// Write a function, howHigh, that takes in the root of a binary tree. The
+// function should return a number representing the height of the tree.
+
+// The height of a binary tree is defined as the maximal number of edges from
+// the root node to any leaf node.
+
+// If the tree is empty, return -1
+
+// parameters
+
+// returns
+
+// examples
+
+// psuedo code
+
+// my recursive code
+// const howHigh = (node) => {
+//   if (node === null) return -1;
+//   const leftSide = howHigh(node.left)
+//   const rightSide = howHigh(node.right)
+//   return Math.max(leftSide, rightSide) + 1
+// };
