3eeb7a28ca23b51a87bb3c2c2ff8e4ccb04f66cc
johnny2smooth <johnbp13@gmail.com>
2022-05-28 20:34:58 -0700

    solution found. prep needed still

--- binaryTree/levelAverages050.js
+++ binaryTree/levelAverages050.js
@@ -1,1 +1,30 @@
 // prep
+
+const levelAverages = (root) => {
+  if (root === undefined) return [];
+  let levels = [];
+  getLevels(root, levels, 0);
+  console.log(levels);
+  return levels.map(getAvgs);
+};
+
+const getAvgs = (array) => {
+  let sum = 0;
+  for (let number of array) {
+    sum += number;
+  }
+  return sum / array.length;
+};
+
+const getLevels = (root, levels, levelNum) => {
+  if (root === null) return [];
+
+  if (levels.length === levelNum) {
+    levels.push([root.val]);
+  } else {
+    levels[levelNum].push(root.val);
+  }
+
+  getLevels(root.left, levels, levelNum + 1);
+  getLevels(root.right, levels, levelNum + 1);
+};
